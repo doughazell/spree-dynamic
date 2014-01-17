d16fd8505ecfae991a63b87d4fadc763b9472d60
Doug Hazell <doughazell@gmail.com>
2014-01-17 12:13:44 +0000

    Tidy up + display bsc spec in admin orders

--- app/overrides/ajax_cart_form.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/products/show',
-                     :name => 'my_remote_cart_form',                
-                     :replace => "code[erb-loud]:contains('form_for :order, :url => populate_orders_path do |f|')",
-                     :erb => "<%= form_for :order, :url => populate_orders_path, :remote => true do |f| %>")
